eec3b2d6414302e7c901f8d2b5a9c112a5c9bad6
Vespera666 <3229027334@qq.com>
2026-07-14 17:55:18 +0800

    2

--- monitorpoint.h
+++ monitorpoint.h
@@ -1,21 +1,22 @@
 #ifndef MONITORPOINT_H
 #define MONITORPOINT_H
 
-#include <QString>
 #include <QDate>
+#include <QString>
 #include "sensor.h"
 
-class MonitoringPoint {
+class MonitoringPoint
+{
 public:
-    QString pointId;             // 监测点编号，如 "MP-W-01"
-    QString sectionName;         // 断面名称，如 "北塔塔顶"
-    QDate   installDate;         // 安装日期
-    QVector <Sensor*> sensors;    // 该监测点安装的所有传感器
-    QString displayName() const;  // 显示名
-    bool hasSensorType(const QString& type) const; // 查询
-    void addSensor(Sensor* s);
+    QString pointId;                               // 监测点编号，如 "MP-W-01"
+    QString sectionName;                           // 断面名称，如 "北塔塔顶"
+    QDate installDate;                             // 安装日期
+    QVector<Sensor *> sensors;                     // 该监测点安装的所有传感器
+    QString displayName() const;                   // 显示名
+    bool hasSensorType(const QString &type) const; // 查询
+    void addSensor(Sensor *s);
     void removeSensor(int index);
-    int  sensorCount() const;
+    int sensorCount() const;
     // 增删传感器
 };
 
--- sensor.cpp
+++ sensor.cpp
@@ -1,6 +1,7 @@
 #include "sensor.h"
 
-QStringList Sensor::headerLabels() const { // 拼接完整表头
+QStringList Sensor::headerLabels() const
+{ // 拼接完整表头
     QStringList headers;
     headers << "时间";
     QStringList names = fieldNames();
@@ -10,6 +11,7 @@ QStringList Sensor::headerLabels() const { // 拼接完整表头
     return headers;
 }
 
-int Sensor::columnCount() const {   // 表格列数：1(时间) + 字段数
+int Sensor::columnCount() const
+{ // 表格列数：1(时间) + 字段数
     return 1 + fieldNames().size();
 }
